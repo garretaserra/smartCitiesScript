fe15b98ddb010fa870473162ba7060caf45bbe6d
sergi <sergi@unievegroup.com>
2020-02-25 19:33:02 +0100

    Almost finished work

--- script.py
+++ script.py
@@ -47,17 +47,30 @@ locationDF = pd.DataFrame({'ID': uniqueIDs, 'Count': allIdCount}, columns=['ID',
 print('Description of distribution of unique ID number of entries:\n', locationDF.describe())
 
 results = pd.DataFrame()
-# Calculate percentage of entries that each WAP is connected when on a given ID
+# Calculate the correlation for each WAP
 for location in uniqueIDs:
     connectivity = []
     for column_name, column_data in data.iloc[:, :-1].iteritems():
-        result = (np.corrcoef((column_data == 1), (data['ID'] == location)))[0, 1]
-        connectivity.append(result)
+        connectivity.append((np.corrcoef((column_data == 1), (data['ID'] == location)))[0, 1])
     results[location] = connectivity
 
 # Rename rows with according WAP names
 for index, column in enumerate(data.columns[0:-1]):
     results.rename(index={index: column}, inplace=True)
 
-print(results.describe())
+# Order results data frame columns
+results = results.reindex(sorted(results.columns), axis=1)
+
+# Find the 5 best WAPs that correlate with all the locations
+for index, column in enumerate(results.columns[0:-1]):
+    print('\nFor ID: ', column)
+    # Print information on the 5 WAPs with more correlation
+    for row in results[column].nlargest(5, keep="first").iteritems():
+        print('\tThe ', row[0], ' has a correlation of ', row[1])
+
+# Get the top 5 WAPs at predicting the ID overall
+print('\n\n\nThe best WAPs at predicting the locations are')
+for row in results.sum(axis=1).nlargest(5, keep='first').iteritems():
+    print('The ', row[0], 'with a value of ', row[1])
+
 plt.show()
