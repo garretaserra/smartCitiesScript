e7db57c11c2e5a8df3e09a5ad9459b6b4df34722
Rocío Jamanca <rozzy.1997@gmail.com>
2020-02-26 17:04:22 +0100

    Comentarios adicionales de clase para script 2.8 Iris_kNN

--- scripts/02_8_iris_kNN.py
+++ scripts/02_8_iris_kNN.py
@@ -1,20 +1,30 @@
-from sklearn import datasets
+import time  # Importar el tiempo
+from sklearn import datasets  # Clasificadores en SK-learn
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
 # Load iris dataset
 iris = datasets.load_iris()
 X = iris.data
-y = iris.target
+y = iris.target  # Etiqueta en la última columna
 
 # Split dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=21, stratify=y)
+# Partir, el número de muestras, de las 150 iniciales unos se utilizaran para test (30%) y  otros para entrenar
+# Todos estos algoritmos llevan una componente aleatoria.
+# random_state=21 (semilla por la cual empieza la secuencia) y para que haga siempre la misma partición.
+# stratify=y va intentar que quede el mismo porcentaje de muestras de cada clase
 
-# Create a k-NN classifier with 5 neighbors
+# Create a k-NN classifier with 5 neighbors (Creamos el clasificador)
 knn = KNeighborsClassifier(n_neighbors=5)
 
-# Fit the classifier to the data
-knn.fit(X_train, y_train)
+# Fit the classifier to the data (Entrenar el clasificador)
+knn.fit(X_train, y_train)  # Se pasan los datos de training y las etiquetas correspondientes.
+# Con el import time
+# t1=time.time()
+# t3=time.time()
+# t12=round(t2-t1,3)
+
 
 # Accuracy
 acc = knn.score(X_test, y_test)
@@ -32,6 +42,3 @@ X_new = [[3.5, 1.2, 4.5, 2.3]]
 y_pred = knn.predict(X_new)
 print("New sample: {}".format(X_new))
 print("New sample prediction: {} {}".format(y_pred, iris.target_names[y_pred]))
-
-
-
