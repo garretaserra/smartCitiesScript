f20195496ec7759a3d6dc26d6a2e0bcfc6d27181
sergi <sergi@unievegroup.com>
2020-02-27 16:42:09 +0100

    Start week2.py

--- week2.py
+++ week2.py
@@ -1,2 +1,11 @@
 import pandas as pd
 import numpy as np
+
+# Read the data from the CSV file
+data = pd.read_csv("datasets/UJIIndoorLoc/UJIIndoorLoc_B0-ID-01.csv")
+
+# KNeighborsClassifier
+# DecisionTreeClassifier
+# GaussianNB
+# SVC
+# MLPClassifier
