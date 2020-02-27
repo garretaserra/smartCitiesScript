645a625d0ff5a8e8806738aaf9799c18d6de98a9
sergi <sergi@unievegroup.com>
2020-02-27 20:15:52 +0100

    Code cleanup

--- week2/DecisionTreeClassifier.py
+++ week2/DecisionTreeClassifier.py
@@ -1,27 +1,16 @@
 # Deliverable S2
 # Classifiers
+# DecisionTreeClassifier
 
 import pandas as pd
 import numpy as np
 import time
-from sklearn import datasets
-from sklearn.model_selection import  train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier
 import matplotlib.pyplot as plt
-from sklearn.tree.export import export_text
-from sklearn.tree import plot_tree
-from scripts.util import plot_decision_regions
 
 # Read the data from the CSV file
 data = pd.read_csv("../datasets/UJIIndoorLoc/UJIIndoorLoc_B0-ID-01.csv")
-
-# 2) Tractem les dades com categories.
-data['ID'] = data['ID'].astype('category')
-data['ID'] = data['ID'].cat.codes
 
 # Print general information of the data
 print('Data information: ', data.info())
