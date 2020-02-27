446156c2c86c0a2b7e97f78b066d022ede167497
sergi <sergi@unievegroup.com>
2020-02-27 18:47:50 +0100

    Started decision tree

--- week2.py
+++ week2.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+from scripts.util import plot_decision_regions
 
 # Read the data from the CSV file
 data = pd.read_csv("datasets/UJIIndoorLoc/UJIIndoorLoc_B0-ID-01.csv")
@@ -15,7 +18,31 @@ print('Labels counts in y_train:', np.bincount(y_train))
 print('Labels counts in y_test:', np.bincount(y_test))
 
 # KNeighborsClassifier
+
+
 # DecisionTreeClassifier
+tree_model = DecisionTreeClassifier(criterion='entropy', max_depth=2, random_state=1)
+tree_model.fit(X_train, y_train)
+# test prediction
+y_pred = tree_model.predict(X_test)
+print('Misclassified samples: %d' % (y_test != y_pred).sum())
+print('Accuracy: %.2f%%' % (100.0 * tree_model.score(X_test, y_test)))
+
+# decision boundary
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+train_len = X_train.shape[0]
+combined_len = X_combined.shape[0]
+
+# plt.figure(figsize=(3, 3), dpi=300)
+# plot_decision_regions(X=X_combined, y=y_combined, classifier=tree_model, test_idx=range(train_len, combined_len))
+# plt.xlabel('petal length [cm]')
+# plt.ylabel('petal width [cm]')
+# plt.legend(loc='upper left')
+# plt.tight_layout()
+# # plt.savefig('images/03_01.png', dpi=300)
+# plt.show()
+
 # GaussianNB
 # SVC
 # MLPClassifier
