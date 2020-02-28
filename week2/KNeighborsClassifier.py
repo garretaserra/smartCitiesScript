5e4c6fa1d11269aa1e236f90c63a48effe4354f9
Rocío Jamanca <rozzy.1997@gmail.com>
2020-02-28 15:19:47 +0100

    KNeighborsClassifier

--- week2/KNeighborsClassifier.py
+++ week2/KNeighborsClassifier.py
@@ -6,7 +6,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
 # Read the data from the CSV file
-data = pd.read_csv("datasets/UJIIndoorLoc/UJIIndoorLoc_B0-ID-01.csv")
+data = pd.read_csv("../datasets/UJIIndoorLoc/UJIIndoorLoc_B0-ID-01.csv")
 
 # 2) Tractem les dades com categories.
 data['ID'] = data['ID'].astype('category')
@@ -58,38 +58,3 @@ print("Test set predictions: {}".format(y_pred))
 print('Well classified samples: {}'.format((y_test == y_pred).sum()))
 print('Misclassified samples: {}'.format((y_test != y_pred).sum()))
 
-
-# plot_decision_regions(X_combined, y_combined, classifier=knn_model, test_idx=range(train_len, combined_len))
-# plt.xlabel('petal length [cm]')
-# plt.ylabel('petal width [cm]')
-# plt.legend(loc='upper left')
-# plt.tight_layout()
-
-
-
-# DecisionTreeClassifier
-# tree_model = DecisionTreeClassifier(criterion='entropy', max_depth=2, random_state=1)
-# tree_model.fit(X_train, y_train)
-# # test prediction
-# y_pred = tree_model.predict(X_test)
-# print('Misclassified samples: %d' % (y_test != y_pred).sum())
-# print('Accuracy: %.2f%%' % (100.0 * tree_model.score(X_test, y_test)))
-
-# decision boundary
-# X_combined = np.vstack((X_train, X_test))
-# y_combined = np.hstack((y_train, y_test))
-# train_len = X_train.shape[0]
-# combined_len = X_combined.shape[0]
-
-# plt.figure(figsize=(3, 3), dpi=300)
-# plot_decision_regions(X=X_combined, y=y_combined, classifier=tree_model, test_idx=range(train_len, combined_len))
-# plt.xlabel('petal length [cm]')
-# plt.ylabel('petal width [cm]')
-# plt.legend(loc='upper left')
-# plt.tight_layout()
-# # plt.savefig('images/03_01.png', dpi=300)
-# plt.show()
-
-# GaussianNB
-# SVC
-# MLPClassifier
