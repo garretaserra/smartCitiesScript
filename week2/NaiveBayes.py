22e396f96ffc88961afea07a253469c2328f5c46
sergi <sergi@unievegroup.com>
2020-03-01 00:46:12 +0100

    Finished Naive Bayes method

--- week2/NaiveBayes.py
+++ week2/NaiveBayes.py
@@ -5,12 +5,15 @@
 import pandas as pd
 import numpy as np
 import time
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from sklearn.naive_bayes import GaussianNB
 
 # Read the data from the CSV file
 data = pd.read_csv("../datasets/UJIIndoorLoc/UJIIndoorLoc_B0-ID-01.csv")
+
+# Remove noise
+data = data.loc[:, (data != 0).any(axis=0)]
 
 # Print general information of the data
 print('Data information: ', data.info())
@@ -21,3 +24,14 @@ print('Size of data: ', data.shape)
 X = data.drop('ID', axis=1)
 y = data['ID']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
+
+# Train model
+gnb_model = GaussianNB()
+gnb_model.fit(X_train, y_train)
+
+# test prediction
+y_pred = gnb_model.predict(X_test)
+print('Misclassified samples: %d' % (y_test != y_pred).sum())
+print('Accuracy: %.2f%%' % (100.0 * gnb_model.score(X_test, y_test)))
+
+plt.show()
