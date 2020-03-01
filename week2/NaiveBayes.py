6b3df2c7d07a8b43c98bfc975180208040298c4d
sergi <sergi@unievegroup.com>
2020-03-01 13:00:28 +0100

    Finished setting up timing in NaiveBayes.py

--- week2/NaiveBayes.py
+++ week2/NaiveBayes.py
@@ -3,7 +3,6 @@
 # NaiveBayes
 
 import pandas as pd
-import numpy as np
 import time
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
@@ -26,11 +25,19 @@ y = data['ID']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
 
 # Train model
+t1 = time.time()
 gnb_model = GaussianNB()
 gnb_model.fit(X_train, y_train)
+t2 = time.time()
+t12 = round(t2-t1, 5)
+print('Time used for training with a size of', len(X_train), 'is', t12)
 
 # test prediction
+t1 = time.time()
 y_pred = gnb_model.predict(X_test)
+t2 = time.time()
+t12 = round(t2-t1, 5)
+print('Time for prediction with size', len(X_test), 'is: ', t12)
 print('Misclassified samples: %d' % (y_test != y_pred).sum())
 print('Accuracy: %.2f%%' % (100.0 * gnb_model.score(X_test, y_test)))
 
