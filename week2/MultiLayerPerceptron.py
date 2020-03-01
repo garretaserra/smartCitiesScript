9898bf638fe8f0a175f5cf82241870845258bbe8
sergi <sergi@unievegroup.com>
2020-03-01 12:54:44 +0100

    Finished setting up timing

--- week2/MultiLayerPerceptron.py
+++ week2/MultiLayerPerceptron.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
+import time
 
 import matplotlib.pyplot as plt
 
@@ -22,10 +22,18 @@ y = data['ID']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
 
 # Train model
-neuralnet_model = MLPClassifier(max_iter=1000)
-neuralnet_model.fit(X_train, y_train)
+t1 = time.time()
+neuralNetworkModel = MLPClassifier(max_iter=1000)
+neuralNetworkModel.fit(X_train, y_train)
+t2 = time.time()
+t12 = round(t2-t1, 5)
+print('Time used for training with a size of', len(X_train), 'is', t12)
 
 # test prediction
-y_pred = neuralnet_model.predict(X_test)
+t1 = time.time()
+y_pred = neuralNetworkModel.predict(X_test)
+t2 = time.time()
+t12 = round(t2-t1, 5)
+print('Time for prediction with size', len(X_test), 'is: ', t12)
 print('Misclassified samples: %d' % (y_test != y_pred).sum())
-print('Accuracy: %.2f%%' % (100.0 * neuralnet_model.score(X_test, y_test)))
+print('Accuracy: %.2f%%' % (100.0 * neuralNetworkModel.score(X_test, y_test)))
