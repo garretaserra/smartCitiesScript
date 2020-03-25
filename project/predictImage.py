506019186d8949279a03598b59599aed95d8c104
garretaserra <garretaserra@gmail.com>
2020-03-25 10:52:22 +0100

    Finished setting up predictor

--- project/predictImage.py
+++ project/predictImage.py
@@ -1,17 +1,14 @@
 import numpy as np
 from keras.preprocessing import image
 from tensorflow_core.python.keras.saving.save import load_model
+import os
 
+test = 'jakes'
+# test = 'trufis'
 
 loaded_model = load_model('./model/model.h5')
-img = image.load_img('./images/dogs/trufis/image0010.jpg', target_size=(200, 200))
-img = np.expand_dims(img, axis=0)
-res = loaded_model.predict_classes(img)
-print(res)
-
-# def predict(picture):
-#     # Load model
-#     return
-#
-#
-# print(predict(image.load_img('./images/dogs/yorkshires/image000.jpg', target_size=(200, 200))))
+for file in os.listdir('./images/dogs/' + test):
+    img = image.load_img('./images/dogs/' + test + '/' + file, target_size=(200, 200))
+    img = np.expand_dims(img, axis=0)
+    res = loaded_model.predict_classes(img)
+    print(res, file)
