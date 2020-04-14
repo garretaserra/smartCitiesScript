4640c4cd675affd83c59dceeeb58db32c04d74c1
garretaserra <garretaserra@gmail.com>
2020-04-14 19:32:02 +0200

    updated predictImage.py automatic size detection and show results

--- project/predictImage.py
+++ project/predictImage.py
@@ -7,8 +7,14 @@ test = 'jakes'
 # test = 'trufis'
 
 loaded_model = load_model('./model/model.h5')
+i = 0
+total = 0
 for file in os.listdir('./images/dogs/' + test):
-    img = image.load_img('./images/dogs/' + test + '/' + file, target_size=(200, 200))
+    img = image.load_img('./images/dogs/' + test + '/' + file, target_size=loaded_model.get_layer(index=0).input_shape[1:3])
     img = np.expand_dims(img, axis=0)
     res = loaded_model.predict_classes(img)
+    if res[0] == 0:
+        i = i + 1
+    total = total + 1
     print(res, file)
+print(i/total)
